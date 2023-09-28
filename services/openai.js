7249d52d1f0baba5ff98d704c9ac41ee8a443eb1
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-09-28 17:29:03 +0530

    refactor: comments

--- services/openai.js
+++ services/openai.js
@@ -1,6 +1,7 @@
 const OpenAI = require("openai");
 require("dotenv").config();
 
+// Initialize openai object with the API key
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 async function generateJSONData(prompt) {
@@ -10,7 +11,9 @@ async function generateJSONData(prompt) {
       prompt: `Select one name of a person which could be an individual from any country and a real existing person, and suggest the designation of the person using the below data and Please organize the URLs of a specific individual in a JSON format, making sure to eliminate any duplicates. Only display the unique URLs for Instagram, LinkedIn, Twitter, and GitHub, and be sure to categorize them based on their respective social platforms. For GitHub, please provide a link to the user profile rather than a repository. Please ensure that the URL values align with their appropriate social platforms, such as using only "instagram.com" for links under Instagram. Additionally, any other urls mentioned should be placed in the "other: []" object from ${prompt}`,
       max_tokens: 256,
     });
-    const data = response.choices[0].text;
+
+    // Extract the data from the response
+    const data = response?.choices[0]?.text;
     return data;
   } catch (error) {
     console.log("Error", error);
--- services/scraper.js
+++ services/scraper.js
@@ -1,9 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function getScrapedData(website) {
-  /**
-   * Set headless to true for @production use
-   */
+  // Set headless to true for @production use
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   const url = website || "https://ayushsoni1010.com/";
@@ -11,27 +9,19 @@ async function getScrapedData(website) {
   try {
     await page.goto(url);
 
-    /**
-     * @screenshot of the portfolio website
-     */
+    // Screenshot of the portfolio website
     // await page.screenshot({ path: "mywebsite.png" });
 
-    /**
-     * @set viewport
-     */
+    //  Set viewport
     await page.setViewport({
       width: 1400,
       height: 800,
     });
 
-    /**
-     * @get the pages HTML content
-     */
+    // Get the pages HTML content
     const pageContent = await page.content();
 
-    /**
-     * @data Handling
-     */
+    // Data Handling
     const [innerText, linkedin, github, twitter, instagram, resume, other] =
       await Promise.all([
         page
@@ -83,27 +73,19 @@ async function getScrapedData(website) {
         ,
       ]);
 
-    /**
-     * @extract potential locations using regular expressions
-     */
+    // Extract potential locations using regular expressions
     const locationRegex = /Location: (.+)/i;
     const location = pageContent.match(locationRegex);
 
-    /**
-     * @extract potential names using regular expressions
-     */
+    // Extract potential names using regular expressions
     const nameRegex = /[A-Z][a-z]+ [A-Z][a-z]+/g;
     const names = pageContent.match(nameRegex);
 
-    /**
-     * @extract potential emails using regular expressions
-     */
+    // Extract potential emails using regular expressions
     const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
     const emails = pageContent.match(emailRegex);
 
-    /**
-     * @extract potential numbers using regular expressions
-     */
+    // Extract potential numbers using regular expressions
     const numberRegex = /\b\d{3}[-.]?\d{3}[-.]?\d{4}\b/g;
     const mobileNumbers = pageContent.match(numberRegex);
 
@@ -124,21 +106,13 @@ async function getScrapedData(website) {
       text: innerText,
     };
 
-    /**
-     * @log and @format JSON object
-     */
     // console.log(JSON.stringify(portfolioData, null, 2));
     // console.log(portfolioData);
     return portfolioData;
   } catch (error) {
-    /**
-     * @error Handling
-     */
     console.error("Error occurred:", error);
   } finally {
-    /**
-     * @close connection
-     */
+    // Close connection
     await browser.close();
   }
 }
