167690265676953a3c5c7b970e60e168853e7125
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-02 02:46:21 +0530

    refactor: server code improvement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import scraperRoutes from "./routes/scraperRoutes.js";
 import { logRequestResponse } from "./middlewares/index.js";
-import { connectMongoDB } from "./config/index.js";
-
-import readline from "readline";
-import { askGenie } from "./services/openaiService.js";
-import { getScrapedData } from "./services/scraperService.js";
+import { connectMongoDB } from "./config/connection.js";
 
 dotenv.config();
 
@@ -24,11 +21,11 @@ app.use(cors());
 // app.use(logRequestResponse("log.txt"));
 
 // Routes
-// app.use("/api", scraperRoutes);
+app.use("/api", scraperRoutes);
 // app.use("/api", openaiRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/", (_req, res) => {
+  res.json({ message: "Hey there!" });
 });
 
 console.log("Hey, TypeScript here :)");
@@ -36,58 +33,3 @@ console.log("Hey, TypeScript here :)");
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-read.question("Please enter website URL: ", async (userInput) => {
-  try {
-    const websiteUrl = "https://" + userInput;
-
-    const scrapedData = await getScrapedData(websiteUrl);
-
-    const { website, title, name, email, mobile, location } = scrapedData;
-
-    const personalInfo = { website, title, name, email, mobile, location };
-
-    const personalJSON = await askGenie(
-      "personal",
-      JSON.stringify(personalInfo)
-    );
-    const linksJSON = await askGenie("links", scrapedData.links);
-    const coreJSON = await askGenie("core", scrapedData.text);
-
-    // Parse the JSON
-    const parsedPersonalInfo = JSON.parse(extractJSONContent(personalJSON));
-    const parsedLinks = JSON.parse(extractJSONContent(linksJSON));
-    const parsedCore = JSON.parse(extractJSONContent(coreJSON));
-
-    const genieAnswer = {
-      website,
-      title,
-      name: parsedPersonalInfo?.name,
-      designation: parsedPersonalInfo?.designation,
-      email: parsedPersonalInfo?.emails,
-      mobile_number: parsedPersonalInfo?.mobile_number,
-      location: parsedPersonalInfo?.location,
-      links: parsedLinks?.links,
-      skills: parsedCore?.skills,
-      core_competencies: parsedCore?.core_competencies,
-    };
-
-    console.log(genieAnswer);
-  } catch (error) {
-    console.error("Error occurred:", error);
-  } finally {
-    read.close();
-  }
-});
-
-function extractJSONContent(jsonString) {
-  const startIndex = jsonString.indexOf("{");
-  const endIndex = jsonString.lastIndexOf("}");
-  const jsonContent = jsonString.substring(startIndex, endIndex + 1);
-  return jsonContent;
-}
