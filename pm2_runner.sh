6ceec3d81c7820c4fe3c516968e94941450c323d
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-03 23:50:41 +0530

    chore: deployment

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-    sudo npm install -g pm2 && pm2 start ./server.js --name server
+    npm install -g pm2 && systemctl start pm2-root
 else
-    sudo pm2 restart server
+    systemctl start pm2-root
 fi
