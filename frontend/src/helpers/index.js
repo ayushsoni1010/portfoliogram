b5ebd2d12b2bfcd5d25fc4d924b542ad98aa0921
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-04 04:38:10 +0530

    fix: production url

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -1,6 +1,6 @@
 const helpers = {
   apiURL:
-    process.env.NODE_ENV !== "development"
+    process.env.NODE_ENV !== "production"
       ? `http://localhost:8000`
       : `http://${window.location.host}`,
 };
