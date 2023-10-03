0023e4252e1d8e1cb62858edacbbd2f71eb3f765
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-03 21:09:41 +0530

    chore: deployment

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api", (_req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
