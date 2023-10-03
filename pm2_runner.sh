5e50b3456aa715c9e476ef3343e98b7a2f98f761
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-04 01:13:52 +0530

    test: runner.sh

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-    npm install -g pm2 && systemctl start pm2-root
+    npm install -g pm2 && systemctl start pm2-ayush
 else
-    systemctl restart pm2-root
+    systemctl restart pm2-ayush
 fi
